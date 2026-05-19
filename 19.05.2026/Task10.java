0ca57fa26825dabc705fcaf6c7f9468d44c4c4de
ThivviyaKrishnakumar <thivviyakrishnakumar@gmail.com>
2026-05-19 16:16:54 +0530

    java operator

---
+++ 19.05.2026/Task10.java
@@ -0,0 +1,10 @@
+class Task10{
+public static void main(String args[]){
+boolean a =true;
+boolean b =false;
+
+System.out.println("a && b : " +(a && b));
+System.out.println("a || b : " + (a || b));
+System.out.println("!a : " +(!a));
+}
+}
